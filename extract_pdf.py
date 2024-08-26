489bc5d6efddd1ce9fbe5316a3f9fd315abc8f58
mohan <bmohanraj91@gmail.com>
2024-08-26 09:53:05 +0530

    first commi

---
+++ extract_pdf.py
@@ -0,0 +1,17 @@
+import pymupdf
+
+file_path = "C:\\Users\\mohan\\Downloads\\file23.pdf"
+
+def get_page_obj():
+    doc = pymupdf.open(file_path)
+    return doc.load_page(0)
+
+def print_text(page):
+    print(page.get_text("text"))
+
+
+def main():
+  print_text(get_page_obj())
+
+if __name__ == '__main__':
+    main()
